7ba1f3bb23338219105ef322aedd327b418d7e40
deadblue <root@dead.blue>
2020-04-14 12:18:00 +0800

    Implement pwd.

--- command/pwd.go
+++ command/pwd.go
@@ -2,8 +2,13 @@ package command
 
 import (
 	"dead.blue/cli115/core"
+	"fmt"
+	"strings"
 )
 
+/*
+"pwd" command print full path of current directory.
+*/
 type PwdCommand struct{}
 
 func (c *PwdCommand) Name() string {
@@ -11,5 +16,18 @@ func (c *PwdCommand) Name() string {
 }
 
 func (c *PwdCommand) Exec(ctx *core.Context, args string) (err error) {
+	sb := strings.Builder{}
+	sb.WriteString("/")
+	for i, v := range ctx.Path.Values() {
+		dir, ok := v.(*core.Dir)
+		if !ok {
+			continue
+		}
+		if i > 0 {
+			sb.WriteString("/")
+		}
+		sb.WriteString(dir.Name)
+	}
+	fmt.Println(sb.String())
 	return nil
 }
